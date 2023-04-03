6465e7740e5b1a66902e196901d7ffbfaa15879a
btcardwell <bryan.cardwell@gmail.com>
2023-04-03 15:16:16 +0000

    Add setup.py content and __init__.py in definitions

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 import setuptools
 
-setuptools.setup()
+setuptools.setup(
+    name='sidm',
+    version='0.1.0',    
+    description='Self-Interacting Dark Matter analysis',
+    url='https://github.com/btcardwell/SIDM',
+    author='Bryan Cardwell',
+    author_email='bryan.cardwell@cern.ch',
+    license='BSD 3-clause',
+    packages=['sidm'],
+)
