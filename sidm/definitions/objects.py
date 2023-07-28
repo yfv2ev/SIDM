84e2df57b18795944bccec63bdac921c9156be25
mariajose2398 <mariajose2398@gmail.com>
2023-07-28 14:21:28 +0000

    Defined a new derived object : Dark photons with egm type lj nearby

--- sidm/definitions/objects.py
+++ sidm/definitions/objects.py
@@ -26,4 +26,5 @@ derived_objs = {
     "egm_ljs": lambda objs: objs["ljs"][(objs["ljs"].muon_n == 0)],
     "matched_genAs": lambda objs, r: objs["genAs"][dR(objs["genAs"], objs["ljs"]) < r]
     "matched_genAs_mu": lambda objs, r: objs["genAs"][dR(objs["genAs"], objs["ljs"][(objs["ljs"].muon_n >= 2)]) < r],
+    "matched_genAs_egm": lambda objs, r: objs["genAs"][dR(objs["genAs"], objs["ljs"][(objs["ljs"].muon_n == 0)]) < r],
 }
