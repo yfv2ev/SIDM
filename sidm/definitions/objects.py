146a2a8023287d9c3d5535e4f2f4cf8820f63262
mariajose2398 <mariajose2398@gmail.com>
2023-07-28 14:30:50 +0000

    Correcte d the syntax error in defining the new derived object

--- sidm/definitions/objects.py
+++ sidm/definitions/objects.py
@@ -24,7 +24,7 @@ primary_objs = {
 derived_objs = {
     "mu_ljs": lambda objs: objs["ljs"][(objs["ljs"].muon_n >= 2)],
     "egm_ljs": lambda objs: objs["ljs"][(objs["ljs"].muon_n == 0)],
-    "matched_genAs": lambda objs, r: objs["genAs"][dR(objs["genAs"], objs["ljs"]) < r]
+    "matched_genAs": lambda objs, r: objs["genAs"][dR(objs["genAs"], objs["ljs"]) < r],
     "matched_genAs_mu": lambda objs, r: objs["genAs"][dR(objs["genAs"], objs["ljs"][(objs["ljs"].muon_n >= 2)]) < r],
     "matched_genAs_egm": lambda objs, r: objs["genAs"][dR(objs["genAs"], objs["ljs"][(objs["ljs"].muon_n == 0)]) < r],
 }
