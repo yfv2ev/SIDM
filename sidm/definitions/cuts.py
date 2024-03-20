f9f7fbd9244fa45eb760aa7ac203cdb3a8efcad3
Madisyn Gamble <m251890@usna.edu>
2024-03-20 16:55:46 +0000

    Working on finding the totals failed that make with ElectronIDP

--- sidm/definitions/cuts.py
+++ sidm/definitions/cuts.py
@@ -33,14 +33,14 @@ obj_cut_defs = {
         "|eta| < 2.4": lambda objs: abs(objs["electrons"].eta) < 2.4,
         #Loose ID = bit 1
         "looseID": lambda objs: check_bit(objs["electrons"].idResults,1),
-        "barrel SigmaIEtaIEtaCut": lambda objs: (objs["electrons"].GsfEleFull5x5SigmaIEtaIEtaCut2d_0) < .0112,
-        "barrel DEtaInSeedCut": lambda objs: (abs(objs["electrons"].GsfEleDEtaInSeedCut2d_0) < .00377),
-        "barrel DPhiInCut": lambda objs: (abs(objs["electrons"].GsfEleDPhiInCut2d_0) < .0884),
-        "barrel InverseCut": lambda objs: (objs["electrons"].GsfEleEInverseMinusPInverseCut2d_0) < .193,
-        "barrel Iso": lambda objs: (objs["electrons"].GsfEleRelPFIsoScaledCut2d_0) < .112,
-        "barrel ConversionVeto": lambda objs: (abs(objs["electrons"].GsfEleConversionVetoCut2d_0) == 1),
-        "barrel H/E": lambda objs: (objs["electrons"].GsfEleHadronicOverEMEnergyScaledCut2d_0) < .05,
-        "barrel MissingHits": lambda objs: (abs(objs["electrons"].GsfEleMissingHitsCut2d_0) < 1), 
+        "barrel SigmaIEtaIEtaCut": lambda objs: (objs["electrons"].GsfEleFull5x5SigmaIEtaIEtaCut_0) < .0112,
+        "barrel DEtaInSeedCut": lambda objs: (abs(objs["electrons"].GsfEleDEtaInSeedCut_0) < .00377),
+        "barrel DPhiInCut": lambda objs: (abs(objs["electrons"].GsfEleDPhiInCut_0) < .0884),
+        "barrel InverseCut": lambda objs: (objs["electrons"].GsfEleEInverseMinusPInverseCut_0) < .193,
+        "barrel Iso": lambda objs: (objs["electrons"].GsfEleRelPFIsoScaledCut_0) < .112,
+        "barrel ConversionVeto": lambda objs: (abs(objs["electrons"].GsfEleConversionVetoCut_0) == 1),
+        "barrel H/E": lambda objs: (objs["electrons"].GsfEleHadronicOverEMEnergyScaledCut_0) < .05,
+        "barrel MissingHits": lambda objs: (abs(objs["electrons"].GsfEleMissingHitsCut_0) < 1), 
     }, 
                                                                                                                                 
                                                                        
