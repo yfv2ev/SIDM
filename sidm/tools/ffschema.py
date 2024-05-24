710c81b23119ca4d5b9b78da8b1371bd25b51831
Carter Patten <yfv2ev@virginia.edu>
2024-05-24 13:58:56 +0000

    Removed commented line

--- sidm/tools/ffschema.py
+++ sidm/tools/ffschema.py
@@ -187,7 +187,6 @@ class FFSchema(BaseSchema):
             # handle non-vector objects
             if obj not in branch_forms:
                 branch_forms[obj] = zip_forms(
-                    #{a: branch_forms.pop(f"{obj}_{a}") for a in attributes if a != "n"}, #Original line ------------------------------ Changed here
                     {a: branch_forms.pop(f"{obj}_{a}") for a in attributes if a != "n" and f"{obj}_{a}" in branch_forms},
                     obj,
                     offsets=offsets
